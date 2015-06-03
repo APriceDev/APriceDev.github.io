384a42a31f406e8ce7bceb2e3c84ded4331f12a7
APriceDev <alanprice.dev1@gmail.com>
2015-06-03 19:57:58 -0400

    update opacity

--- js/app.js
+++ js/app.js
@@ -4,16 +4,18 @@ var lissajous = (function(){
 
     var scope, canvasCtx;
 
-    var drawSine = function(){
+    var drawLogo = function(){
 
-        var x, y, amt = 400, offsetX = scope.width/2, offsetY = scope.height/2, amp = 6, sz = 2;
+        var x, y, amt = 400, offsetX = scope.width/2 -25, offsetY = scope.height/2 , amp = 6, sz = 2 , opacity = 0.8;
 
         for (var i = 0; i <amt; i++) {
 
             x = Math.round(amp + i/25 * (Math.sin(i) * 10) + offsetX);
             y = Math.round(amp + i/30  * (Math.cos(i) * 10) + offsetY);
-            canvasCtx.fillStyle = "rgba(41,45,48,1)";
+            canvasCtx.fillStyle = "rgba(41, 45, 48, " + opacity + ")";
             canvasCtx.fillRect(x, y, sz, sz);
+
+            opacity -= 0.002;
         };
     };
 
@@ -22,7 +24,7 @@ var lissajous = (function(){
         scope = document.getElementById("scope"),
         canvasCtx = scope.getContext("2d");
 
-        drawSine();
+        drawLogo();
     };
 
     return {
